cc4314099f961f7cbe2429e5419f60824e7b6287
蛍 <93361361+minamikana-git@users.noreply.github.com>
2023-03-08 14:21:46 +0900

    first commit

---
+++ src/main/java/org/hotal/treeassister/SneakHandler.java
@@ -0,0 +1,131 @@
+package org.hotal.treeassister;
+
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.type.Sapling;
+import org.bukkit.entity.Ageable;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SneakHandler implements Listener {
+    private final Map<String, PlayerState> states = new HashMap<>();
+
+    private static final ItemStack boneMeal = new ItemStack(Material.BONE_MEAL);
+    private static final Object nmsBoneMeal = ReflectionUtil.itemStackAsNmsCopy(boneMeal);
+
+    private final Random rnd = new Random();
+
+    public static class PlayerState {
+        public boolean isSneaking;
+        public boolean isSneaked;
+        public int ticksLastCheck;
+    }
+
+    @EventHandler
+    public void onSneak(PlayerToggleSneakEvent event) {
+        onAction(event.getPlayer());
+    }
+
+    private void onAction(Player player) {
+        PlayerState state = states.computeIfAbsent(player.getName(), e -> new PlayerState());
+
+        int ticksNow = Bukkit.getCurrentTick();
+        int ticksSinceLastCheck = ticksNow - state.ticksLastCheck;
+        if (ticksSinceLastCheck >= TreeAssister.cooldown) {
+            state.ticksLastCheck = ticksNow;
+
+            World world = player.getWorld();
+            Object nmsWorld = ReflectionUtil.craftWorldGetHandle(world);
+            Location location = player.getLocation();
+
+            if (state.isSneaked) {
+                // 作物成長
+                {
+                    List<Block> ageables = getAgeableBlockInRange(location, TreeAssister.blockRadius);
+
+                    ageables.forEach(block -> {
+                        if (rnd.nextFloat() < TreeAssister.blockPercentage) {
+                            Object nmsBlockPosition = ReflectionUtil.constructBlockPosition(block.getX(), block.getY(), block.getZ());
+                            ReflectionUtil.applyBoneMeal(nmsBoneMeal, nmsWorld, nmsBlockPosition);
+
+                            if (TreeAssister.showParticles)
+                                sendPacketGrowBlock(block.getLocation());
+                        }
+                    });
+                }
+
+                // Mob成長
+                {
+                    List<Ageable> ageables = getAgeableEntityInRange(location, TreeAssister.mobRadius);
+
+                    ageables.forEach(entity -> {
+                        if (rnd.nextFloat() < TreeAssister.mobPercentage) {
+                            entity.setAge(entity.getAge() + 1);
+                            entity.setBreed(true);
+
+                            if (TreeAssister.showParticles)
+                                sendPacketGrowEntity(entity.getLocation());
+                        }
+                    });
+                }
+
+                state.isSneaked = false;
+            }
+        }
+
+        boolean isSneaking = player.isSneaking();
+        if (state.isSneaking != isSneaking) {
+            state.isSneaking = isSneaking;
+            state.isSneaked = true;
+        }
+    }
+
+    private void sendPacketGrowBlock(Location location) {
+        Collection<Player> players = location.getNearbyPlayers(48);
+        for (Player player : players)
+            player.playEffect(location, Effect.VILLAGER_PLANT_GROW, Integer.valueOf(0));
+    }
+
+    private void sendPacketGrowEntity(Location location) {
+        Collection<Player> players = location.getNearbyPlayers(48);
+        for (Player player : players)
+            player.spawnParticle(Particle.VILLAGER_HAPPY, location, 5, .2, .2, .2);
+    }
+
+    private List<Block> getAgeableBlockInRange(Location location, int radius) {
+        World world = location.getWorld();
+        int centerX = location.getBlockX();
+        int centerY = location.getBlockY();
+        int centerZ = location.getBlockZ();
+        List<Block> list = new ArrayList<>();
+        for (int x = -radius; x <= radius; x++)
+            for (int y = -2; y <= 2; y++)
+                for (int z = -radius; z <= radius; z++) {
+                    Block block = world.getBlockAt(x + centerX, y + centerY, z + centerZ);
+                    BlockData blockData = block.getBlockData();
+                    if (TreeAssister.enableSaplings && blockData instanceof Sapling) {
+                        list.add(block);
+                    } else if (TreeAssister.enableCrops && blockData instanceof org.bukkit.block.data.Ageable) {
+                        org.bukkit.block.data.Ageable data = (org.bukkit.block.data.Ageable) blockData;
+                        if (data.getAge() < data.getMaximumAge())
+                            list.add(block);
+                    }
+                }
+        return list;
+    }
+
+    private List<org.bukkit.entity.Ageable> getAgeableEntityInRange(Location location, int radius) {
+        return location.getNearbyLivingEntities(radius).stream()
+                .filter(e -> e instanceof org.bukkit.entity.Ageable)
+                .map(org.bukkit.entity.Ageable.class::cast)
+                .filter(e -> !e.isAdult() || !e.canBreed())
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/org/hotal/treeassister/TreeAssister.java
@@ -0,0 +1,43 @@
+package org.hotal.treeassister;
+
+import org.bukkit.configuration.Configuration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public final class TreeAssister extends JavaPlugin {
+    public static Logger log;
+
+    public static int cooldown;
+    public static boolean enableSaplings;
+    public static boolean enableCrops;
+    public static boolean showParticles;
+    public static int blockRadius;
+    public static int mobRadius;
+    public static double blockPercentage;
+    public static double mobPercentage;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        log = getLogger();
+
+        Configuration config = getConfig();
+        cooldown = config.getInt("Tweaks.cooldownTicks", 5);
+        enableSaplings = config.getBoolean("Tweaks.growSaplings", true);
+        enableCrops = config.getBoolean("Tweaks.growCrops", true);
+        showParticles = config.getBoolean("Tweaks.showParticles", true);
+        blockRadius = config.getInt("Tweaks.blockRadius", 5);
+        mobRadius = config.getInt("Tweaks.mobRadius", 5);
+        blockPercentage = config.getDouble("Tweaks.blockPercentage", 0.1);
+        mobPercentage = config.getDouble("Tweaks.mobPercentage", 0.05);
+        saveDefaultConfig();
+
+        getServer().getPluginManager().registerEvents(new SneakHandler(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
